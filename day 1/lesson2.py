20a4921eca3183a3764ea549f2a5c9cd977a78c0
Alex090-yup <asamushia1@gmail.com>
2024-07-03 15:33:37 +0400

    commit1

---
+++ day 1/lesson2.py
@@ -0,0 +1,7 @@
+name = "alex"
+surname = "samushia"
+age = "14"
+height = "1.69"
+favourite_food = "kebab"
+hobby = "taekwon-do"
+print(name + " " + surname + " " + age + " " + height + " " + favourite_food + " " + hobby)
