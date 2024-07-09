db3e1b53c63b87e950a00cb4b32b80fd5850aeb5
Alex090-yup <asamushia1@gmail.com>
2024-07-09 22:52:24 +0400

    day 4/classwork/level6.py

---
+++ day 4/classwork/task.py
@@ -0,0 +1,12 @@
+git_hub = "Alex77"
+print(git_hub)
+number = 4
+print(number)
+print(6 + 1)
+print (9 / 3)
+print (7*7)
+# не хватает кавычек к слову "cow"
+animal = cow
+
+#этим кодом мы получаем ответ 8 (число будет показано в терминале)
+print (9-1)
