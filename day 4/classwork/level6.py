b06d725ea3d0d9fd21f4bf331421a3e2e68cbe64
Alex090-yup <asamushia1@gmail.com>
2024-07-09 21:30:58 +0400

    day 4/classwork/level6.py

--- day 4/classwork/level6.py
+++ day 4/classwork/level6.py
@@ -1,3 +1,10 @@
 print ( 5 + 4)
 print (12 / 4) # делиться на цело
 print (12 // 4) # показывает сколько раз 4 входит в 12 
+age = 1 + 4
+print (age)
+
+age1 = 5
+age2 = 6
+totaleage = age1 + age2
+print (totaleage)
