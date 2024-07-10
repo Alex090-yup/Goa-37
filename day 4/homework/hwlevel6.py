b4afea41a374534277656d14ecf8ca087195eb16
Alex090-yup <asamushia1@gmail.com>
2024-07-10 12:08:17 +0400

    day 4/homework/hwlevel6.py

---
+++ day 4/homework/hwlevel6.py
@@ -0,0 +1,24 @@
+# я написал команду с помощю snake case. Эта команда выводит в терминале слово "git_hub"
+git_hub = "Alex090-yup"
+print(git_hub)
+
+print(1987+4567)
+print(1234*456)
+print(4/4)
+print(5678-2347)
+print (1/3 * 1/4)
+
+number1= 4
+number2= 8
+print(number1 + number2)
+print(number1 // number2)
+print(number1 - number2)
+print(number1 * number2)
+
+#სავარჯიშოები:
+#1. რომელი არის ცვლადის შექმნის სწორი გზა?:
+#ბ) dog_name
+#2. რას ნიშნავს case sensitive ენა?
+#Это язык, в котором различаються буквы. Например: "Hello!" и "hello!" и т.д
+#3. რა კარგი პრაქტიკები ვიცით პითონში?
+# коментарии (только это запомнил)
