25458dec8e1aaf00899aae34e4004ce833483238
Alex090-yup <asamushia1@gmail.com>
2024-07-26 13:21:41 +0400

    new file:   Goa 39/level10/homework/homework1.py new file:   Goa 39/level10/homework/homework2.py

---
+++ Goa 39/level10/homework/homework1.py
@@ -0,0 +1,29 @@
+#10 примеров and:
+print(True and False )
+print(False and False)
+print(False and True)
+print(True and True)
+print(8>6 and 3>6)
+print(20<10 and 8<10)
+print(8==8 and 6==4)
+print(9<=9 and 10<=7)
+print(9>=5 and 50>=50)
+
+#особый пример:
+equation = 6!=6
+print(equation and 90!=40)
+
+#10 примеров or:
+print(True or False )
+print(False or False)
+print(False or True)
+print(True or True)
+print(8>6 or 3>6)
+print(20<10 or 10<9)
+print(8==8 or 6==4)
+print(9<=9 or 10<=7)
+print(9>=5 or 50>=50)
+
+#особый пример:
+equation = 6!=7 
+print(equation or 90!=40)
---
+++ Goa 39/level10/homework/homework2.py
@@ -0,0 +1,41 @@
+# 5 примеров > :
+print(4>5)
+print(10>7)
+print(5>9)
+print(3>0)
+print(4>0.78)
+
+# 5 примеров < :
+print(6<8)
+print(8<9)
+print(9<0)
+print(0.6<0.8)
+print(10<10.1)
+
+# 5 примеров <= :
+print(8<=6)
+print(9<=9)
+print(0<=10)
+print(8<=4)
+print(0.678<=0.679)
+
+# 5 примеров >= :
+print(6>=6)
+print(7>=4)
+print(8>=9)
+print(7>=4)
+print(0.9>=1)
+
+# 5 примеров == :
+print(6==6)
+print(7==8)
+print(5==6)
+print(4==6)
+print(0.86759576==0.86759576)
+
+# 5 примеров != :
+print(3!=4)
+print(6!=5)
+print(3!=3)
+print(3.7!=6)
+print(3.4!=3.4)
