a15b3565a01c14ecbc9903830baf38a5e7ec75f0
Alex090-yup <asamushia1@gmail.com>
2024-08-15 12:56:56 +0400

    modified:   .DS_Store 	new file:   Goa 39/level16/homework/homework1.py

---
+++ Goa 39/level16/homework/homework1.py
@@ -0,0 +1,23 @@
+i=100
+while i<120:
+    print('Scam')
+    i= i+9
+
+i=100
+while i>50:
+    print('Hello!')
+    i=i-2
+
+i=int(input())
+while i!=34:
+    i=int(input())
+
+i=100
+while i>34:
+    print('y')
+    i=i-1
+
+i=45
+while i<46:
+    print(5)
+    i=i+5
