a4b65d3990a4e04895dbfc48e02e61b97c68068c
Alex090-yup <asamushia1@gmail.com>
2024-08-15 22:58:43 +0400

    new file:   Goa 39/level17/classwork/classwork1.py

---
+++ Goa 39/level17/classwork/classwork1.py
@@ -0,0 +1,21 @@
+number=0
+while number<10:
+   print(number)
+   number=number+1
+
+number=20
+while number>10:
+    print(number)
+    number=number-1
+
+
+name=0
+while name<5:
+    print('alex')
+    name=name+1
+
+password='Sicret'
+password1=input()
+while password!=password1:
+    password1=input()
+print('Match')
