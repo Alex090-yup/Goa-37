d09cb993db0e61d5c016cafbe2bc53e5743d08a8
Alex090-yup <asamushia1@gmail.com>
2024-08-22 22:57:56 +0400

    modified:   Goa 39/level19/classwork/classwork1.py 	new file:   Goa 39/level19/classwork/classwork2.py

---
+++ Goa 39/level19/classwork/classwork1.py
@@ -0,0 +1,7 @@
+num=13
+if num<18:
+    print('underage')
+elif num==18:
+    print('teen')
+elif num>18:
+    print('adult')
---
+++ Goa 39/level19/classwork/classwork2.py
@@ -0,0 +1,7 @@
+num= int(input('Write a number: '))
+if num<0:
+    print('negative')
+elif num>0:
+    print('positive')
+elif num==0:
+    print('zero')
