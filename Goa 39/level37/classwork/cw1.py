92a5e2bfa8975a9cddd4257bc7d2136ee022a5f8
Alex090-yup <asamushia1@gmail.com>
2024-10-24 22:19:37 +0400

    new file:   Goa 39/level37/classwork/cw1.py

---
+++ Goa 39/level37/classwork/cw1.py
@@ -0,0 +1,10 @@
+#* შექმენი ფუნქცია სახელად max_of_two, რომელიც 
+#არგუმენტად იღებს ორ რიცხვს. 
+
+#ფუნქციამ უნდა დააბრუნოს და დაპრინტოს ამ
+#ორი რიცხვიდან უფრო დიდი. 
+
+def max_of_two (num1,num2):
+    compararison= max(num1,num2)
+    return  compararison
+print( max_of_two(1234,5678))
