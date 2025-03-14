68e2ef4c692e17802f544c7a929707d96a5022e3
Alex090-yup <asamushia1@gmail.com>
2025-03-15 01:45:03 +0400

    new file:   Goa 39/level077/homework/website/image/image.jpg

---
+++ Goa 39/level078/classwork/script.js
@@ -0,0 +1,7 @@
+function getInput(){
+    let input = document.getElementById('input').value;
+    let p = document.getElementById('p');
+    p.textContent = input;
+    console.log(input)
+}
+getInput()
