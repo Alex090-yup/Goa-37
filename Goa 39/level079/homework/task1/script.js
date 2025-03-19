1a73490a1724893804762170b8ad9307af365fed
Alex090-yup <asamushia1@gmail.com>
2025-03-19 23:48:55 +0400

    new file:   Goa 39/level079/homework/task1/index.html

---
+++ Goa 39/level079/homework/task1/script.js
@@ -0,0 +1,21 @@
+function allInfType(){
+    let quesion = confirm('Want you live the site?')
+    let plus = 5+5
+    let minus = 18-7
+    let multiply = 4*2
+    let deleniya = 6/3
+    let string = 'Total: '
+    let x = plus + minus
+    let y = multiply * deleniya
+    let z = string + x
+    let a = plus + minus < multiply * deleniya
+    let b = plus+multiply > minus - deleniya
+    alert(x)
+    alert(y)
+    alert(z)
+    alert(a)
+    alert(b)
+    console.log(a)
+    console.log(b)
+}
+allInfType()
