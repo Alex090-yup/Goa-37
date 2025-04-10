c455bbbed785117ed047a89a66ef510a8daccc86
Alex090-yup <asamushia1@gmail.com>
2025-04-10 23:00:11 +0400

    modified:   Goa 39/.DS_Store

---
+++ Goa 39/level086/classwork/script.js
@@ -0,0 +1,21 @@
+count=0
+
+function plus1(){
+    count=10
+    document.getElementById('counter').textContent= count
+}
+
+function plus2(){
+    count=20
+    document.getElementById('counter').textContent= count
+}
+
+function plus3(){
+    count=30
+    document.getElementById('counter').textContent= count
+}
+
+function plus4(){
+    count=40
+    document.getElementById('counter').textContent= count
+}
