162a28e81cb460b718efeb4cb1fa463d6625dd13
Alex090-yup <asamushia1@gmail.com>
2025-05-22 23:06:55 +0400

    new file:   Goa 39/level097/classwork/ image.jpg

---
+++ Goa 39/level097/classwork/script.js
@@ -0,0 +1,29 @@
+function changeWidth(){
+    let img = document.getElementById('img');
+    img.style.width = '400px';
+}
+
+function changeHeight(){
+    let img = document.getElementById('img');
+    img.style.height = '300px';
+}
+
+function changeBorderRadius(){
+    let img = document.getElementById('img');
+    img.style.borderRadius = '10px';
+}
+
+function changeBoxShadow(){
+    let img = document.getElementById('img');
+    img.style.boxShadow = '5px 5px 50px 1px green';
+}
+
+function changeColor(){
+    let h1 = document.getElementById('h1')
+    h1.style.color = 'red'
+}
+
+function changeSize(){
+    let h1 = document.getElementById('h1')
+    h1.style.fontSize = '60px'
+}
