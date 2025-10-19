32bcde9a5ab079e951f67d8e04b2d3e66160c657
Alex090-yup <asamushia1@gmail.com>
2025-10-19 16:57:53 +0400

    new file:   group 19-20-33/level88/classwork/script1.js

---
+++ group 19-20-33/level88/classwork/script4.js
@@ -0,0 +1,8 @@
+let fruits = ["banana", "melon", "apple", "cherry"]
+let numbers = [1, 2, 3, 4, 5, 6]
+let floats = [2.5, 5.6, 7.6]
+let booleans = [true, false]
+
+let total = [fruits, numbers, floats, booleans]
+
+console.log(total)
---
+++ group 19-20-33/level88/classwork/script5.js
@@ -0,0 +1,2 @@
+let hg = ["alex", "fi", 1, 2, [2, 4]]
+let text = ["j", "h"]
