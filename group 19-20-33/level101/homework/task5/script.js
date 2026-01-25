96d16bbc083f961e1ae2a4513662a64aceeaa67b
Alex090-yup <asamushia1@gmail.com>
2026-01-25 15:41:08 +0400

    modified:   group 19-20-33/level101/homework/task5/script.js

--- group 19-20-33/level101/homework/task5/script.js
+++ group 19-20-33/level101/homework/task5/script.js
@@ -1,13 +1,11 @@
 let lst = [
-    {brand: "Toyota", year: 2004}, 
-    {num1: 1, num2: 2},
-    {team1: "Munchester United", team2: "Munchester City"},
-    {word1: "hello", word2: "bye"},
-    {food1: "meat", food2: "tomato"}
+    {name: "Toyota", year: 2004}, 
+    {name: 1, num2: 2},
+    {name: "Munchester United", team2: "Munchester City"},
+    {name: "hello", word2: "bye"},
+    {name: "meat", food2: "tomato"}
 ]
 
 for(let i = 0; i<lst.length; i++){
-    for(let key in lst[i]){
-        console.log(lst[i][key])
-    }
+    console.log(lst[i]["name"])
 }
