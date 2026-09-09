7e9593d572f99cf2b1a4b7c31afa33da335db77b
Alex090-yup <asamushia1@gmail.com>
2026-09-09 19:43:26 +0400

    modified:   group 19-20-33/level127/homework/task2/index.html

---
+++ group 19-20-33/level127/homework/task2/script.js
@@ -0,0 +1,20 @@
+let div = document.getElementById("img-text")
+
+setInterval(() =>{
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    if(seconds<10){
+        seconds = `0${seconds}`
+    }
+    if(minutes<10){
+        minutes = `0${minutes}`
+    }
+    if(hours<10){
+        hours = `0${hours}`
+    }
+    
+    div.textContent = `${hours}:${minutes}:${seconds}`
+})
